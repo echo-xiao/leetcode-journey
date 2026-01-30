27c3c02b8a5e2fbe4957b3f2601bd19346da426b
echo-xiao <56253351+echo-xiao@users.noreply.github.com>
2026-01-29 19:52:05 -0800

    Delete Problems directory

--- Problems/26_remove-duplicates-from-sorted-array/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        slow = 0
-        for fast in range(0, len(nums)):
-            if nums[slow] != nums[fast]:
-                slow += 1
-                nums[slow] = nums[fast]
-        return len(nums[0:slow+1])
